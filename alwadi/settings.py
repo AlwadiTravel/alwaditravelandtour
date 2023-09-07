c7e49b28b694e3f39493ab33ef39ecbadf124a9a
AlwadiTravel <alwaditraveleri@gmail.com>
2023-09-07 13:25:09 +0300

    database change

--- alwadi/settings.py
+++ alwadi/settings.py
@@ -72,9 +72,13 @@ WSGI_APPLICATION = 'alwadi.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'alwadi',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
